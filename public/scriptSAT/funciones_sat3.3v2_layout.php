781ded8364d5d5e829f5f54b0c6231043b4317fa
Mario Villatoro <ing.mario.villatoro@gmail.com>
2020-05-04 16:01:30 -0500

    se hace validacion de subsidio para el empleo (otros pagos)

--- public/scriptSAT/funciones_sat3.3v2_layout.php
+++ public/scriptSAT/funciones_sat3.3v2_layout.php
@@ -541,8 +541,17 @@ $Dato="DC|3.3|NOM".$TipoNomina."|".$Folio."|".$FechaHoraGeneracion."|99|".number
     fwrite($fh,$Dato.PHP_EOL);
     }
 
-    $Dato="NOP|002|999|Subsidio para el empleo (efectivamente entregado al trabajador)|0.01|0.01|||\r";
-    fwrite($fh,$Dato.PHP_EOL);
+    if($row_srcSQL["OTROS_PAGOS"] == 0.01){
+		$Dato="NOP|002|999|Subsidio para el empleo (efectivamente entregado al trabajador)|0.01|0.01|||\r";
+        fwrite($fh,$Dato.PHP_EOL);
+	}else if($row_srcSQL["OTROS_PAGOS"] > 0){
+        $Dato="NOP|999|19999|Otros Pagos|".number_format($row_srcSQL["OTROS_PAGOS"],2,".","")."||||\r";
+        //$Dato="NOP|999|P24GA|GRATIFICACIÓN DE FIN DE AÑO|".number_format($row_srcSQL["OTROS_PAGOS"],2,".","")."||||\r";
+        fwrite($fh,$Dato.PHP_EOL);
+    }
+    
+    //$Dato="NOP|002|999|Subsidio para el empleo (efectivamente entregado al trabajador)|0.01|0.01|||\r";
+    //fwrite($fh,$Dato.PHP_EOL);
     
 	/*if($row_srcSQL["OTROS_PAGOS"] > 0){
     $Dato="NOP|999|19999|Otros Pagos|".number_format($row_srcSQL["OTROS_PAGOS"],2,".","")."||||\r";
