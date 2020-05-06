c7959aafc839002b6540a9dc6c704ac5108a83c3
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2020-05-06 11:11:47 -0500

    correcion en otros pagos

--- public/scriptSAT/funciones_sat3.3v2_layout.php
+++ public/scriptSAT/funciones_sat3.3v2_layout.php
@@ -548,6 +548,9 @@ $Dato="DC|3.3|NOM".$TipoNomina."|".$Folio."|".$FechaHoraGeneracion."|99|".number
         $Dato="NOP|999|19999|Otros Pagos|".number_format($row_srcSQL["OTROS_PAGOS"],2,".","")."||||\r";
         //$Dato="NOP|999|P24GA|GRATIFICACIÓN DE FIN DE AÑO|".number_format($row_srcSQL["OTROS_PAGOS"],2,".","")."||||\r";
         fwrite($fh,$Dato.PHP_EOL);
+        
+        $Dato="NOP|002|999|Subsidio para el empleo (efectivamente entregado al trabajador)|0.00|0.00|||\r";
+        fwrite($fh,$Dato.PHP_EOL);
     }
     
     //$Dato="NOP|002|999|Subsidio para el empleo (efectivamente entregado al trabajador)|0.01|0.01|||\r";
